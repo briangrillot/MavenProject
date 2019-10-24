9d87d9fea76d1e28c226dda9d754c02a440b01e7
Pankace <briangrillot@hotmail.fr>
2019-10-24 14:02:23 +0200

    DevOps TP

---
+++ src/main/java/etu/ut2j/test/MavenTP/App.java
@@ -0,0 +1,13 @@
+package etu.ut2j.test.MavenTP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
